0133c13091cdc20c3ae3b0441300e7fbbd247a14
shygodb <qibingfang@gmail.com>
2019-06-19 18:45:07 +0800

    fine-tuning goods-view in bosshome

--- mall-view/src/components/bosshome/good/Goodview.vue
+++ mall-view/src/components/bosshome/good/Goodview.vue
@@ -72,10 +72,6 @@
                 <template slot-scope="scope">
                     <el-button
                     size="mini"
-                    @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-
-                    <el-button
-                    size="mini"
                     type="danger"
                     @click="handleDelete(scope.$index, scope.row)">删除</el-button>
                 </template>
